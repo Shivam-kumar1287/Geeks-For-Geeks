8f5d5d0e7ee13433bb07a71f89bec601a99ccb71
Shivam-Kumar-108 <kumarshiva34014@gamil.com>
2025-04-13 23:22:29 +0530

    13-04-2025

---
+++ MissingInArray.java
@@ -0,0 +1,26 @@
+    
+/*  :>) Question Link   */
+/* */
+// User function Template for Java
+
+class Solution {
+    int missingNumber(int arr[]) {
+        // code here
+       long l=arr.length+1;
+       long sum1=l*(l+1)/2;
+        long sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        
+        return (int) (sum1-sum);
+        
+    }
+}
+public class MissingInArray {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {1, 2, 4, 5};
+        System.out.println(sol.missingNumber(arr)); // Output: 3
+    }
+}
