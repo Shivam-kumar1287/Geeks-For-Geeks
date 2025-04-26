5cacae7d05ba41fdf519427095de45208878e572
Shivam-kumar1287 <kshivam0508@gmail.com-to-Shivam-kumar1287.com>
2025-04-26 22:59:46 +0530

    Add Arrays changes

--- Array/IndexesofSubarraySum.java
+++ Array/IndexesofSubarraySum.java
@@ -1,29 +1,43 @@
-public class IndexesofSubarraySum {
-    
-}
-
-// /* question links :>) https://www.geeksforgeeks.org/find-subarray-with-given-sum/ */
-
-import java.util.*;
-
-class Solution {
-    
-    static ArrayList<Integer> subarraySum(int[] arr, int target) {
-        // code here
-        ArrayList<Integer> ans=new ArrayList<>();
-        int n=arr.length,j=0,sum=0;
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
-            while(sum>target && j<i) sum-=arr[j++];
-            if(sum==target){
-                ans.add(j+1);
-                ans.add(i+1);
-                return ans;
-            }
-    
-        }
-        ans.add(-1);
-        return ans;
+class IndexesOfSubarraySum
+{
+    public static void main(String args[])
+    {
+        int arr[] = {1,2,3,4,5};
+        int sum = 9;
+        int n = arr.length;
+        subarraySum(arr, n, sum);
     }
-}
-
+    
+    public static void subarraySum(int arr[], int n, int sum)
+    {
+    int curr_sum = arr[0];
+    int start = 0;
+    int end = 0;
+    for(int i = 1; i < n; i++)
+    {
+        while(curr_sum > sum && start < i)
+        {
+            curr_sum = curr_sum - arr[start];
+            start++;
+        }
+        if(curr_sum == sum)
+        {
+            end = i;
+            break;
+        }
+        if(i < n)
+        {
+            curr_sum = curr_sum + arr[i];
+        }
+    }
+    if(curr_sum == sum){
+        System.out.println("Subarray found from index " + start + " to " + end);
+    }
+    else
+    {
+        System.out.println("No subarray found with the given sum.");
+    }
+    }
+    }   
+        
+    
